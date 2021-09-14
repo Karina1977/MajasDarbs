adb72da35026a0c654aaeb698248fda8a4b6c523
Homer <Homer@DESKTOP-LKI2RBT>
2021-09-14 12:37:46 +0300

    Mainn

---
+++ MajasDarbs/MajasDarbs/ProgramBase.cs
@@ -0,0 +1,46 @@
+﻿namespace MajasDarbs
+{
+    internal class ProgramBase
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hi!");
+
+            Console.WriteLine("Kā tevi sauc?");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Sveiks, " + userName);
+
+            Console.WriteLine("ļoti patīkami iepazītes!");
+
+            Console.WriteLine("Kur tu dzīvo?");
+
+            string userName1 = Console.ReadLine();
+
+            Console.WriteLine("Skaidrs!");
+
+            Console.WriteLine("Cik tev ir gadi?");
+
+            string ageText = Console.ReadLine();
+            int age = int.Parse(ageText);
+            int previosYearAge = age - 1;
+
+            Console.WriteLine("Sveiks tev ieprekšēja gadā bija:  " + previosYearAge);
+
+
+            Console.WriteLine("Prieks iepazītes!");
+
+            Console.WriteLine();
+
+
+
+            //--------------------------------------
+
+
+
+        }{
+    }
+}
